cb11068d72fcc815b68c20c742bc74e71636e123
JiangJiafu <jiangjiafu1989@qq.com>
2017-09-02 11:21:34 +0800

    使用矩阵运算来优化计算距离的性能

--- Digit Recognition/my_knn.py
+++ Digit Recognition/my_knn.py
@@ -1,9 +1,8 @@
 # encoding=utf8
-import heapq
 import cProfile
 import numpy as np
 import csv
-import matplotlib.pyplot as plt
+import operator
 
 
 def toInt(array):
@@ -27,42 +26,32 @@ def nomalizing(array):
     return array
 
 
-def read_train_data():
+def load_train_data():
     lst = []
     with open('train.csv', 'r') as csv_file:
         lines = csv.reader(csv_file)
         num = 0
         for line in lines:  # 先只用前10000条训练集
             lst.append(line)  # 42001*785
-            num += 1
-            if num >= 10001:
-                break
-        lst.remove(lst[0])  # remove the head line
+            # num += 1
+            # if num > 4000:
+            #     break
+        del (lst[0])
         lst = np.array(lst)
         label = lst[:, 0]
         data = lst[:, 1:]
-        return nomalizing(toInt(data)), toInt(label)  # label 1*42000  data 42000*784
+        return nomalizing(toInt(data)), toInt(label)  # label 42000*1  data 42000*784
 
 
-def read_test_data():
+def load_test_data():
+    lst = []
     with open('test.csv', 'r') as csv_file:
-        return csv.reader(csv_file)
-
-
-def my_knn(k=10):
-    # read the training data and label
-    train_data, labels = read_train_data()
-    csv_file2 = open('test.csv', 'r')
-    test_data_list = csv.reader(csv_file2)
-    is_head_line = True
-    i = 0
-    for test_record in test_data_list:
-        if is_head_line:
-            is_head_line = False
-            continue
-        if i in [9, 10, 11, 12, 13]:
-            print 'test record %d, result:%d ' % (i, calculate(k, test_record, train_data, labels))
-        i += 1
+        lines = csv.reader(csv_file)
+        for line in lines:
+            lst.append(line)
+    del (lst[0])
+    data = np.array(lst)
+    return nomalizing(toInt(data))
 
 
 def __cmd(a, b):
@@ -75,7 +64,7 @@ def __cmd(a, b):
 
 
 def calculate_similarity(test_record, train_record):
-    """calculate the test record and the train record.
+    """calculate the similarity of test record and the train record.
     
     Returns:
         the similarity value.
@@ -94,26 +83,32 @@ def calculate_similarity(test_record, train_record):
     return value
 
 
-def calculate(k, test_record, train_data, labels):
+def old_classify(k, test_record, train_data, labels):
+    """
+    :param k: 
+    :param test_record: 1 * n 
+    :param train_data: m * n
+    :param labels: m * 1
+    :return: classifiy result
+    """
     value_list = []
-    test_record = np.array(test_record, dtype='uint8')
     j = 0
     record_num = len(train_data)
     for i in range(record_num):
         train_record = train_data[i]
         # print for test
         j += 1
-        if j % 1000 == 0:
-            print 'j:%d' % j
-            print 'len value_list:%d' % len(value_list)
+        # if j % 1000 == 0:
+        #     print 'j:%d' % j
+        # print 'len value_list:%d' % len(value_list)
         value = calculate_similarity(test_record, train_record)
         if value <= 0:
             continue
         value_list.append({'value': value, 'label': labels[0][i]})
-    print value_list
+    # print value_list
     value_list.sort(cmp=__cmd)
     value_list = value_list[0:k]
-    print 'value list2:', value_list
+    # print 'value list2:', value_list
     label0_num = len([x for x in value_list if x['label'] == 0])
     label1_num = len([x for x in value_list if x['label'] == 1])
     label2_num = len([x for x in value_list if x['label'] == 2])
@@ -134,6 +129,58 @@ def calculate(k, test_record, train_data, labels):
             max_label_num = label_num[i]
 
     return ret
+
+
+def saveResult(result):
+    with open('result.csv', 'wb') as my_file:
+        my_writer = csv.writer(my_file)
+        my_writer.writerow(['ImageId', 'Label'])
+        for i in range(len(result)):
+            my_writer.writerow([i + 1, result[i]])
+
+
+def classify(k, test_record, train_data, labels):
+    """
+    :param k: 
+    :param test_record: 1 * n 
+    :param train_data: m * n
+    :param labels: m * 1
+    :return: classifiy result
+    """
+
+    test_record = np.mat(test_record)
+    data_set = np.mat(train_data)
+    labels = np.mat(labels)
+    data_set_size = data_set.shape[0]
+    diff_mat = np.tile(test_record, (data_set_size, 1)) - data_set
+    sq_diff_mat = np.array(diff_mat) ** 2
+    sq_distances = sq_diff_mat.sum(axis=1)
+    distances = sq_distances ** 0.5
+    sorted_dist_indicies = distances.argsort()
+    class_count = {}
+    for i in range(k):
+        voteIlabel = labels[0, sorted_dist_indicies[i]]
+        class_count[voteIlabel] = class_count.get(voteIlabel, 0) + 1
+    sorted_class_count = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
+
+
+def my_knn(k=10):
+    # read the training data and label
+    train_data, labels = load_train_data()
+    test_data = load_test_data()
+    m, n = np.shape(test_data)
+    result_list = []
+    for i in range(m):
+        classifier_result = classify(k, test_data[i], train_data, labels)
+        result_list.append(classifier_result)
+        print 'test record %d, result:%d ' % (i, classifier_result)
+        i += 1
+        if i % 1000 == 0:
+            print '%d %% finished' % (i * 100 / m)
+        # if i == 2:
+        #     break
+    saveResult(result_list)
 
 
 if __name__ == '__main__':
